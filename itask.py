aeb2a7bdaa075aaeddd11da351e1b25fc0d11dcc
JonaRL <jonathanr@online.de>
2021-03-09 10:51:19 +0100

    Fix mistakes that led to errors

--- itask.py
+++ itask.py
@@ -6,9 +6,9 @@ import functools
 import os
 import platform
 
-from . import task as showtask
-from . import gettask
-from . import setupwizard as setup
+import task as showtask
+import gettask
+import setuptool as setup
 
 if platform.system() == "Linux":
   datafolder = os.path.join(str(Path.home()), ".itask")
